15b0d91a035189efe9cf66cde4170580f209828b
Petter Kraus <petterkraus@gmail.com>
2022-12-15 20:27:56 -0300

    Basic Layout

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -4,9 +4,9 @@ export default function Navbar() {
             <h3 className="font-poppins text-2xl font-bold px-6">Petter Kraus</h3>
             <nav className="">
                 <ul className="flex gap-6 font-semibold">
-                    <li className="cursor-pointer hover:text-fuchsia-500">Projetos</li>
-                    <li className="cursor-pointer hover:text-fuchsia-500">Sobre</li>
-                    <li className="bg-fuchsia-900 w-20 text-center h-7 rounded-full cursor-pointer hover:bg-fuchsia-700">Contato</li>
+                    <li className="cursor-pointer hover:text-fuchsia-500">Projects</li>
+                    <li className="cursor-pointer hover:text-fuchsia-500">About</li>
+                    <li className="bg-fuchsia-900 w-20 text-center h-7 rounded-full cursor-pointer hover:bg-fuchsia-700">Contact</li>
                 </ul>
             </nav>
             <h1 className="pr-12">{' '}</h1>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,42 @@
+import Image from 'next/image'
+import Profile from '../assets/profile.png'
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+
 export default function Home() {
     return (
-        <div className="bg-slate-200 h-[calc(100vh-86px)]">
-            <h1 className="text-3xl font-bold pt-8 pl-6">
-                Hello world!
-            </h1>
+        <div className="bg-slate-200 h-[calc(100vh-86px)] grid grid-cols-2 items-center">
+            <div className='flex w-1/2 items-center mx-auto'>
+                <aside className='pr-8'>
+                    <ul className='text-4xl flex flex-col gap-7 text-fuchsia-700'>
+                        <li className='cursor-pointer' >
+                            <FaLinkedin />
+                        </li>
+                        <li className='cursor-pointer'>
+                            <FaGithubSquare />
+                        </li>
+
+                    </ul>
+                </aside>
+
+                <div className=''>
+                    <h1 className="text-6xl font-bold ">
+                        Hello! I'm Petter
+                    </h1>
+                    <p className='text-2xl pt-2 text-slate-700 font-semibold'>FullStack developer</p>
+                    <p className='pt-8 text-xl'>
+                        Full stack developer focused on front-end, with experience in React.JS, seeking to offer quality in my work                    </p>
+
+                </div>
+            </div>
+            <div className='items-start'>
+                <Image
+                    src={Profile}
+                    width={600}
+                    height={600}
+                    alt='Logo do NextJS'
+
+                />
+            </div>
         </div>
     )
 }
